23c344d36787acb174d8ad1eea5f7a7c2edbb3fe
Mohammad Abbas Abidi <109078342+MdAbbas110@users.noreply.github.com>
2024-03-03 23:52:44 +0530

    Create currying.js
    added impot question

---
+++ javaScript_prepes/currying.js
@@ -0,0 +1,13 @@
+/*
+Implement this
+ sum(a)(b)(c)…. (n) through currying in js
+Ques2 - sum(1,2,3…..n)(1)(2)
+Ques 3 - Sum(1,2,3…..n)(1,2……n
+*/
+
+function sum () {
+
+}
+
+console.log(sum(1,2)(4))
+console.log(sum(1,2)(4))
